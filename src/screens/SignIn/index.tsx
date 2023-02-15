c897989d55e240f73e97f4f09f043de71bdd5e92
Filipi Rafael <filipirafael.123@gmail.com>
2023-02-15 17:50:24 -0300

    Add signup page

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,13 +1,76 @@
-import { View, Image } from 'react-native';
+import {
+  View,
+  SafeAreaView,
+  Image,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from 'react-native';
 import { styles } from './styles';
+
+import Brand from '../../assets/brand.svg';
 
 export const SignIn = () => {
   return (
-    <View style={styles.container}>
-      <Image
-        source={require('../../assets/loginbg.png')}
-        style={styles.image}
-      />
-    </View>
+    <KeyboardAvoidingView
+      behavior='position'
+      style={styles.container}
+    >
+      <View style={styles.wrapper}>
+        <Image
+          source={require('../../assets/loginbg.png')}
+          style={styles.image}
+        />
+
+        <Brand style={styles.brand} />
+
+        <Text style={styles.title}>
+          The best way to predict the future is to create it
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Access your account
+        </Text>
+
+        <TextInput
+          placeholder='Email'
+          keyboardType='email-address'
+          placeholderTextColor='#808080'
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder='Password'
+          keyboardType='visible-password'
+          placeholderTextColor='#808080'
+          style={styles.input}
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Text
+            style={styles.textButton}
+          >
+            Access
+          </Text>
+        </TouchableOpacity>
+
+        <View style={styles.signWrapper}>
+          <Text
+            style={[styles.textButton, styles.signText]}
+          >
+            Any account?
+          </Text>
+
+          <Text style={[styles.signText, styles.signTextLink]}>
+            Sign Up here
+          </Text>
+        </View>
+
+      </View>
+    </KeyboardAvoidingView>
   )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -7,8 +7,70 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#0D0D0D',
   },
+  wrapper: {
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  },
   image: {
     width,
     height,
+    position: 'absolute'
+  },
+  brand: {
+    marginTop: height / 18
+  },
+  title: {
+    color: '#E1E1E6',
+    fontSize: 12,
+    marginTop: 16
+  },
+  subtitle: {
+    color: '#E1E1E6',
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: height / 6,
+    marginBottom: 20
+  },
+  input: {
+    width: '100%',
+    height: 60,
+    backgroundColor: '#0D0D0D',
+    color: '#FFF',
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 10,
+    borderWidth: 1,
+    borderColor: '#333333'
+  },
+  button: {
+    width: '100%',
+    height: 60,
+    backgroundColor: '#03D361',
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15
+  },
+  textButton: {
+    color: '#FFF',
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  signWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20
+  },
+  signText: {
+    fontSize: 16,
+  },
+  signTextLink: {
+    color: '#03D361',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    marginLeft: 10
   }
 });
