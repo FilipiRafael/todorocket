a50c9ee05d5b72571d28d6f50cec647f09347f31
Filipi Rafael <filipirafael.123@gmail.com>
2023-02-26 16:46:14 -0300

    Removed stripe and added payment link

--- src/screens/Support/index.tsx
+++ src/screens/Support/index.tsx
@@ -6,8 +6,6 @@ import { styles } from './styles';
 import { useNavigation } from '@react-navigation/native';
 import { LinearGradient } from 'expo-linear-gradient';
 
-import { Stripe } from '../../services/stripe';
-
 import LottieView from 'lottie-react-native';
 
 export const Support = () => {
@@ -16,45 +14,45 @@ export const Support = () => {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Stripe>
-        <LinearGradient
-          colors={['#220b2d', '#0D0D0D']}
-          style={styles.background}
-        />
-        <AntDesign
-          name='back'
-          style={styles.goBack}
-          color='#8257E6'
-          size={26}
-          onPress={goBack}
-        />
+      <LinearGradient
+        colors={['#220b2d', '#0D0D0D']}
+        style={styles.background}
+      />
+      <AntDesign
+        name='back'
+        style={styles.goBack}
+        color='#8257E6'
+        size={26}
+        onPress={goBack}
+      />
 
-        <Text style={styles.title}>
-          Buy me a coffee
-        </Text>
+      <Text style={styles.title}>
+        Buy me a coffee
+      </Text>
 
-        <TouchableOpacity
-          style={styles.button}
-          activeOpacity={0.7}
-          onPress={() => Linking.openURL('https://donate.stripe.com/test_7sI5lH9pocXLg6c8ww')}
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+        onPress={() => {
+          Linking.openURL('https://donate.stripe.com/3csbL99RG0Sp0mIeUV');
+        }}
+      >
+        <Text
+          style={styles.buttonText}
         >
-          <Text
-            style={styles.buttonText}
-          >
-            Support $1
-          </Text>
-        </TouchableOpacity>
+          Support $1
+        </Text>
+      </TouchableOpacity>
 
-        <LottieView
-          source={require('../../assets/developer.json')}
-          style={styles.animation}
-          ref={animation}
-          progress={1}
-          speed={1}
-          autoPlay
-          loop
-        />
-      </Stripe>
+      <LottieView
+        source={require('../../assets/developer.json')}
+        style={styles.animation}
+        ref={animation}
+        progress={1}
+        speed={1}
+        autoPlay
+        loop
+      />
     </SafeAreaView>
   )
 }
