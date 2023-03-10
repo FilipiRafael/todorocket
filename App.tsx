9f82b36765a5ed888fdb5cc9657f5539bc3cd992
Filipi Rafael <filipirafael.123@gmail.com>
2023-03-10 16:49:35 -0300

    Added Sentry to verify crashes and performance

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
 import 'react-native-gesture-handler';
+import * as Sentry from 'sentry-expo';
 import { StatusBar } from 'expo-status-bar';
 import { Routes } from './src/routes';
 
 import { AuthProvider } from './src/contexts/auth';
 import { TasksProvider } from './src/contexts/tasks';
 
-export default function App() {
+Sentry.init({
+  dsn: "https://f84d6f08882e4a838fc73d3bf98be274@o4504815931162624.ingest.sentry.io/4504815940272128",
+  enableInExpoDevelopment: true,
+  debug: true,
+  tracesSampleRate: 1.0
+});
+
+function App() {
   return (
     <AuthProvider>
       <TasksProvider>
@@ -19,3 +27,5 @@ export default function App() {
     </AuthProvider>
   );
 }
+
+export default App;
